40d77d2d641e1a38cc7a550f135101df269bfb3d
Neeraj <neerajtripathi7447@gmail.com>
2022-07-12 11:50:21 +0530

    V1

--- src/com/company/arrays/arrays2D_input.java
+++ src/com/company/arrays/arrays2D_input.java
@@ -1,2 +1,34 @@
-package com.company.arrays;public class arrays2D_input {
+package com.company.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arrays2D_input {
+    //explanation there in DSA ipad
+    public static void main(String[] args) {
+        int arr[] [] = new int[3][2];
+        System.out.println("Length of the arrays is "+arr.length);
+
+        //defining the input
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int row = 0; row<arr.length;row++){
+            for(int col =0; col<arr[0].length;col++){
+                //taking input of the values
+                arr[row][col] = sc.nextInt();
+            }
+
+        }
+
+        //printing the array
+
+        for (int i = 0; i <arr.length ; i++) {
+
+            System.out.println("Arrays at "+i+" position is "+ Arrays.toString(arr[i]));
+
+        }
+
+
+    }
 }
